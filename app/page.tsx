878363e9662b35a7ed9197a5c93be61d8e04969d
churi14 <julian.pogonza13@gmail.com>
2026-03-04 13:08:12 -0300

    cambios nuevos

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import CategoryGrid from '@/components/CategoryGrid'; // <-- Importamos el archivo nuevo
-
+import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
       <CategoryGrid /> {/* <-- Lo agregamos a la página */}
+      <Footer />
     </main>
   );
 }
